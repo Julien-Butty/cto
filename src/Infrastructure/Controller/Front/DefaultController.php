3fbd8e7509728f7a5c7d0c70720a76804c946b00
Julien Butty <julien.butty@novaway.fr>
2021-12-14 22:49:20 +0100

    feat: init  Default Controller

---
+++ src/Infrastructure/Controller/Front/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Infrastructure\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('homepage.html.twig');
+    }
+
+}
---
+++ tests/DefaultControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testDefaultPage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', "Hello World");
+    }
+}
