8561771b0bf217205a40fce4edd7f3a05cb48b38
Julien Butty <julien.butty@novaway.fr>
2021-12-23 22:59:56 +0100

    feat: add User entity

---
+++ src/Infrastructure/Doctrine/Entity/DoctrineUser.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Infrastructure\Doctrine\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[Entity]
+class DoctrineUser implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[Id]
+    #[GeneratedValue]
+    #[Column(type: Types::INTEGER)]
+    private int $id;
+
+    #[Column(type: Types::STRING, length: 180, unique: true)]
+    private string $email;
+
+    #[Column(type: Types::JSON)]
+    private array $roles = [];
+
+    #[Column(type: Types::STRING)]
+    private string $password;
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function eraseCredentials()
+    {
+
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+}
